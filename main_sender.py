c458f1b316670077ec33203f7df82784698c23e9
goindi <45494818+goindi@users.noreply.github.com>
2018-12-10 13:01:26 -0500

    A process for sending message
    This is the main asynchronous sender process.
    Sleeps for 300 ms between calls (not sure if needed)

---
+++ main_sender.py
@@ -0,0 +1,44 @@
+"""
+Symphony Bot to listen and send messages
+By - Harmeet Goindi
+@ Trade Alert
+"""
+import logging
+from  logging.handlers import RotatingFileHandler
+import symphony_config as cfg
+import redis
+import SymphonyClass 
+import time
+
+logger = logging.getLogger(__name__)
+formatter=logging.Formatter('[%(funcName)s:%(lineno)d] [%(levelname)s %(asctime)s] %(message)s')
+debughandler = RotatingFileHandler(cfg.debuglogfile,maxBytes=20971520,backupCount=10)
+debughandler.setLevel(logging.DEBUG)
+debughandler.setFormatter(formatter)
+logger.addHandler(debughandler)
+
+infohandler = RotatingFileHandler(cfg.infologfile,maxBytes=20971520,backupCount=10)
+infohandler.setLevel(logging.INFO)
+infohandler.setFormatter(formatter)
+logger.addHandler(infohandler)
+
+errorhandler = RotatingFileHandler(cfg.errorlogfile,maxBytes=5242880,backupCount=10)
+errorhandler.setLevel(logging.ERROR)
+errorhandler.setFormatter(formatter)
+logger.addHandler(errorhandler)
+logger.setLevel(logging.DEBUG)
+
+r = redis.StrictRedis(host=cfg.redis_host, port=cfg.redis_port, db=cfg.redis_db)
+symph_instance=SymphonyClass.SymphonyBot(cfg,logger,r)
+
+
+while True:
+    
+    symph_instance.read_files_and_send_reply()
+    time.sleep(0.3)
+    
+
+
+    
+    
+    
